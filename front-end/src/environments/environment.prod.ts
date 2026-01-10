d2c85241972a6454aeaf09c0430e105d3d8d1aab
Giovanny López <giovanny.lopez@igac.gov.co>
2026-01-09 21:11:59 -0500

    Se ajusta docker-compose 9

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-api-url.com/api/v1',
-  oauthUrl: 'https://your-api-url.com/oauth2/authorization',
-  googleMapsApiKey: 'TU_CLAVE_DE_PRODUCCION_AQUI'
+  apiUrl: 'http://auth-service:8080/api/v1',
+  oauthUrl: 'http://auth-service:8080/oauth2/authorization',
+  googleMapsApiKey: 'AIzaSyD1b5MGfkcYRB20XdbsvrBdST5gA51pZpI',
+  footballApiKey: 'c4029806cbd1a1a4cdb25107398464d4'
 };
