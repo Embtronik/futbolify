b5ea1e88a65298840378c9f490c5eb607c727201
Giovanny López <giovanny.lopez@igac.gov.co>
2026-01-22 11:23:49 -0500

    se ajusta environment

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: '/api/v1',
   oauthUrl: '/oauth2/authorization',
-  teamsApiUrl: '/api/teams',
+  teamsApiUrl: '/api',
   googleMapsApiKey: 'AIzaSyD1b5MGfkcYRB20XdbsvrBdST5gA51pZpI',
   footballApiKey: 'c4029806cbd1a1a4cdb25107398464d4'
 };
