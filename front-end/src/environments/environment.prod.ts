5aa607a3c8df58abd41944b1b8bad74b8d22550b
Giovanny López <giovanny.lopez@igac.gov.co>
2026-01-22 09:43:26 -0500

    Se ajsta en docker-compose.yml 7

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: '/api/v1',
   oauthUrl: '/oauth2/authorization',
-  teamsApiUrl: '/api',
+  teamsApiUrl: '/api/teams',
   googleMapsApiKey: 'AIzaSyD1b5MGfkcYRB20XdbsvrBdST5gA51pZpI',
   footballApiKey: 'c4029806cbd1a1a4cdb25107398464d4'
 };
